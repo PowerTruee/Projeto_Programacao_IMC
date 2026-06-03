f1686ee3a8b6438112717b44e9d0c168a5026aed
PowerTruee <kaianossesmansim@gmail.com>
2026-06-03 09:17:11 -0300

    V2

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("NutriFit Advisor")
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
+
+imc = peso / (altura ** 2)
+print(f"\nSeu IMC é: {imc:.2f}")
